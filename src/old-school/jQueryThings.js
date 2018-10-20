3afa36973d8a024ecab40be15e81ed5f6b52bdc7
Jasmine Francois <francois.jasminem@gmail.com>
2018-10-19 20:03:10 -0500

    Updates styles and improved functionality of checking an item off the list

--- src/old-school/jQueryThings.js
+++ src/old-school/jQueryThings.js
@@ -1,28 +1,53 @@
-const todoList = [];
+let toDoList = [];
+let finishedToDoList = [];
+
+$(document).ready(() => {
+    $('.to-do-input').bind("keyup", (event) => {
+        if (event.keyCode === 13) {
+            addItemToList();
+        }
+    });
+});
 
 function addItemToList() {
     const listItem = $('.to-do-input').val();
     if (listItem.length) {
-        todoList.push(listItem);
+        toDoList.push(listItem);
 
-        const toDoList = buildToDoList();
-        $('.to-do-list-items').html(toDoList);
+        const newToDoList = buildToDoList();
+        $('.to-do-list-items').html(newToDoList);
     }
 
     $('.to-do-input').val('');
 }
 
 function buildToDoList() {
-    return todoList.map((listItem) => {
-        return `<li>${listItem}<i class="fas fa-times-circle delete-button" onclick="deleteListItem('${listItem}')"> </i></li>`;
+    return toDoList.map((listItem) => {
+        return `<div class="to-do-list-item">
+                    <li class="list-item-text">${listItem}</li> 
+                    <i class="far fa-circle delete-button" onClick="deleteListItem('${listItem}')"> </i>
+                </div>`;
     });
 }
 
 function deleteListItem(listItem) {
-    const listItemIndex = todoList.indexOf(listItem);
-    todoList.splice(listItemIndex, 1);
+    const listItemIndex = toDoList.indexOf(listItem);
+    toDoList.splice(listItemIndex, 1);
 
     const modifiedToDoList = buildToDoList();
-
     $('.to-do-list-items').html(modifiedToDoList);
+
+    finishedToDoList.push(listItem);
+
+    const modifiedFinishedToDoList = buildFinishedToDoList();
+    $('.finished-to-do-list-items').html(modifiedFinishedToDoList);
+}
+
+function buildFinishedToDoList() {
+    return finishedToDoList.map((listItem) => {
+        return `<div class="finished-to-do-list-item">
+                    <li class="finished-list-item-text">${listItem}</li> 
+                    <i class="far fa-check-circle finished-delete-button"> </i>
+                </div>`;
+    });
 }
